5ec67e85c7cc5227d8bf8f72cb48c64672a65cb1
83Fatma <fatma.ethar@gmail.com>
2023-03-31 13:11:06 +0200

    Versión 1

--- AR402223/src/Actividad/Operaciones.java
+++ AR402223/src/Actividad/Operaciones.java
@@ -2,8 +2,17 @@ package Actividad;
 
 import java.util.Scanner;
 
-public class Operaciones {
+//-----------
+//programa que realiza las operaciones matimaticas
+//
+//@Fatma
+//31/3/2023
+//-------------
 
+public class Operaciones {
+	
+
+	//constructor
 	public Operaciones() {
 		
 		
@@ -22,23 +31,35 @@ public class Operaciones {
 		resMultiplicar =0 ;
 		Scanner teclado = new Scanner(System.in);
 		
-		System.out.println("Inrudocca un nombero entero positivo: ");		
+		//mostrar al usuario mensaje 
+		System.out.println("Inrudocca un nombero entero positivo: ");
+		
+	    //leer numero entero por el teclado
 		numero1 = teclado.nextInt() ;
 		
-		System.out.println("Inrudocca un nombero entero positivo: ");		
+		//mostrar al usuario mensaje 
+		System.out.println("Inrudocca un nombero entero positivo: ");
+		
+		//leer numero entero por el teclado
 		numero2 = teclado.nextInt() ;
 		
+		//comprobar si el numero positivo o no
 		if((numero1 > 0)  && (numero2 > 0)) {
+			
+			//Sumar los dos numeros
 			suma = numero1 + numero2;
 			
+			//Restar los dos numeros
 			resta = numero1 - numero2;
 			
+			//Multiplicar los dos numeros
 			resMultiplicar =  numero1 * numero2;
 			
+			//Devidir los dos numeros
 			resDivision =  numero1 / numero2;
 		}
 
-		
+		//mostrar los resultados en pantalla
 		System.out.println("la suma es :  " + suma);
 		System.out.println("la resta es :  " + resta);
 		System.out.println("la multiplicar es :  " + resMultiplicar);
